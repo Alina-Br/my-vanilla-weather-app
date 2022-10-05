a41fef274f45441b1e8fdca3973ef10fccd622ca
Alina-Br <britalinka@gmail.com>
2022-10-05 21:46:06 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "6dc434835335b6fc7f29ab4642dd33e5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
