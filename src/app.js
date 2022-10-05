601d071872903d2b0f0c45591228a067b3a732fd
Alina-Br <britalinka@gmail.com>
2022-10-05 18:46:32 +0300

    minimum temperature rounding

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  minTempElement.innerHTML = response.data.main.temp_min;
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
 
   console.log(response.data);
 }
