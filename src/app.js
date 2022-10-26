8076fa8f0371a70a7dabb22fbd947fad7869e93c
Alina-Br <britalinka@gmail.com>
2022-10-26 18:54:57 +0300

    valid API key

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "6dc434835335b6fc7f29ab4642dd33e5";
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -106,7 +106,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "6dc434835335b6fc7f29ab4642dd33e5";
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
